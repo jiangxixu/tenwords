d101e056ab48ec980e4bacf9fa98657eb5f77bb2
jiangxixu <143887676+jiangxixu@users.noreply.github.com>
2026-09-13 20:26:49 +0800

    Update PWA cache version for daily vocabulary files

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-const CACHE='tenwords-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./data/words.json','./manifest.webmanifest','./favicon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
+const CACHE='tenwords-v3';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./data/words.json','./data/daily/index.json','./manifest.webmanifest','./favicon.svg'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return r;
+  }).catch(()=>caches.match(e.request)));
+});
